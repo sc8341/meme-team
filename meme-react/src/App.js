63fa87628280af4a4b9973c724d447cc4c04d583
sc8341 <sc8341@nyu.edu>
2022-12-30 23:10:26 -0500

    test fetch meme api

--- meme-react/src/App.js
+++ meme-react/src/App.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
   const [data, setData] = useState([{}])
+  const [memes, setMemes] = useState([{}])
+  const [memeIndex, setMemeIndex] = useState(0);
 
   useEffect(() => {
     fetch("/test").then(
@@ -16,16 +18,35 @@ function App() {
     )
   }, [])
 
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes').then(
+      res => res.json().then( res => {
+        const memes = res.data.memes;
+        setMemes(memes);
+      })
+    )
+  }, [])
+
   return (
-    <div>
-      {(typeof data.test === 'undefined') ? (
-        <p>Loading...</p>
-      ):(
-        data.test.map((ele, i) => (
-          <p key={i}>{ele}</p>
-        ))
-      )}
-    </div>
+    <>
+      <div>
+        {(typeof data.test === 'undefined') ? (
+          <p>Loading...</p>
+        ):(
+          data.test.map((ele, i) => (
+            <p key={i}>{ele}</p>
+          ))
+        )}
+      </div>
+      <div>
+        <button onClick={() => setMemeIndex(memeIndex+1)}>Generate</button>
+        {(memes.length===0) ? (
+          <img src='http://spittoon.co.uk/wp-content/uploads/2019/01/error-meme.jpg' alt='error meme'/>
+        ):(
+          <img src={memes[memeIndex].url} />
+        )}
+      </div>
+    </>
   );
 }
 
