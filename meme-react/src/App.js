ab8e21028fda202b9971d1b37b17f7f8554d312e
sc8341 <sc8341@nyu.edu>
2022-12-30 22:34:32 -0500

    fetched test data into react

--- meme-react/src/App.js
+++ meme-react/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/test").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(typeof data.test === 'undefined') ? (
+        <p>Loading...</p>
+      ):(
+        data.test.map((ele, i) => (
+          <p key={i}>{ele}</p>
+        ))
+      )}
     </div>
   );
 }
